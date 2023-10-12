a3e02613428446fbf23dfdf2416b838416efc252
Selvam <arumugaselvam23052003@gmail.com>
2023-10-12 14:27:49 +0530

    updated

--- Backend/Routes/menuRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const Router = express.Router({ mergeParams: true });
-
-const menuController = require("./../Controllers/menuController");
-
-Router.route("/").get(menuController.getAllMenu);
-
-module.exports = Router;
--- Backend/Routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const Router = express.Router();
-
-const orderController = require("./../Controllers/orderController");
-
-Router.get("/", orderController.getAllOrder);
-
-module.exports = Router;
